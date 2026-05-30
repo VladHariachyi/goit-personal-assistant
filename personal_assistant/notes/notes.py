87282458acd104dbe1526e3b46921f366ffae8d5
Olena Yenkova <oyenkova@sapi-tech.com>
2026-05-30 17:05:57 +0300

    feat/PA-15-Search-Notes-by_Tags

--- personal_assistant/notes/notes.py
+++ personal_assistant/notes/notes.py
@@ -1,2 +1,38 @@
-class Notes:
-    pass
+class Note:
+
+    def __init__(
+        self,
+        text,
+        tags=None
+    ):
+
+        self.text = text
+
+        self.tags = tags or []  # Якщо tags не передали створюється пустий список.
+
+    def add_tag(self, tag):
+
+        tag = tag.lower()
+
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def remove_tag(self, tag):
+
+        tag = tag.lower()
+
+        if tag in self.tags:
+            self.tags.remove(tag)
+
+    def has_tag(self, tag):
+
+        return tag.lower() in self.tags
+
+    def __str__(self):
+
+        tags = ", ".join(self.tags)
+
+        return (
+            f"Note: {self.text}\n"
+            f"Tags: {tags}"
+        )
